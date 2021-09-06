1c3d8a145ebf1a695a933bc4363f9b47cd4f0a78
黄晰维 <736962639@qq.com>
2021-09-06 10:48:24 +0800

    完成

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <!-- keepalive的使用使得单页面应用不被销毁 -->
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
+    <main-tab-bar/>
+  </div>
+</template>
+
+<script>
+  import MainTabBar from 'components/content/mainTabbar/MainTabBar'
+
+  export default {
+    name: 'app',
+    components: {
+      MainTabBar
+    }
+  }
+</script>
+
+<style>
+  @import "assets/css/base.css";
+</style>
